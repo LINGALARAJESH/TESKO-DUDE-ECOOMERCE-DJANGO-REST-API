6c1c9968342f3b1f1ba2fbd088b1d1b9894bd505
LINGALARAJESH <rajeshlingala08@gmail.com>
2024-09-17 21:45:32 +0530

    hai..

--- teskobackend/settings.py
+++ teskobackend/settings.py
@@ -198,6 +198,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
+
 MEDIA_ROOT='static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
